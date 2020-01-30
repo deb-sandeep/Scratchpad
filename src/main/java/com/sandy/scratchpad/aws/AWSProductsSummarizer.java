46ca543dd17584e3706f63d0dd27014db8a6f382
Sandeep Deb <deb.sandeep@gmail.com>
2020-01-30 21:07:28 +0530

    Split chapters per category. More chewable.

--- src/main/java/com/sandy/scratchpad/aws/AWSProductsSummarizer.java
+++ src/main/java/com/sandy/scratchpad/aws/AWSProductsSummarizer.java
@@ -14,30 +14,97 @@ import org.apache.log4j.Logger ;
 public class AWSProductsSummarizer {
     
     private static final Logger log = Logger.getLogger( AWSProductsSummarizer.class ) ;
+    private FileWriter fw = null ;
+    
+    private File jnFolder = new File( "/Users/sandeep/Documents/StudyNotes/JoveNotes-IT/Sandeep/Cloud/01 - Provider Products" ) ;
     
     public void execute() throws Exception {
         Map<String, List<AWSProductMeta>> metaMap = null ;
         
         metaMap = getProductMeta() ;
+        createJoveNotes( metaMap ) ;
+    }
+    
+    private void createJoveNotes( Map<String, List<AWSProductMeta>> metaMap ) 
+        throws Exception {
         
-        FileWriter fw = new FileWriter( "/home/sandeep/temp/AWSProducts.txt" ) ;
+        int subChapterNumber = 1 ;
+        
         for( String category : metaMap.keySet() ) {
             log.debug( "Category = " + category ) ;
-            fw.write( "Category : " + category + "\n" + 
-                      "------------------------------------------\n\n" ) ; 
-            
             List<AWSProductMeta> metaList = metaMap.get( category ) ;
-            for( AWSProductMeta meta : metaList ) {
-                fw.write( "# " + meta.getProductName() + "\n\n" ) ;
-                fw.write( "[" + meta.getProductCategory() + "]\n"  ) ;
-                fw.write( "[" + meta.getUrl() + "]\n\n"  ) ;
-                fw.write( meta.getShortDescription() + "\n\n" ) ;
-                fw.write( meta.getLongDescription() + "\n" ) ;
-                fw.flush() ;
+            if( metaList.size() > 3 ) {
+                createJN( category, metaList, subChapterNumber ) ;
+                subChapterNumber++ ;
             }
         }
+    }
+    
+    private void createJN( String category, 
+                           List<AWSProductMeta> metaList, 
+                           int subChapterNumber ) 
+        throws Exception {
+        
+        File jnFile = getJNFile( category, subChapterNumber ) ;
+        fw = new FileWriter( jnFile ) ;
+        
+        fw.write( "subject \"Cloud\"\n" ) ;
+        fw.write( "chapterNumber 1." + subChapterNumber + "\n" ) ;
+        fw.write( "chapterName \"AWS - " + category + "\"\n" ) ;
+        fw.write( "\n" ) ;
+        
+        createMatchingQuestion( metaList ) ;
+        
+        for( AWSProductMeta meta : metaList ) {
+            writeQuestion( "Which product does this?\n\n**" + meta.getShortDescription() + "**" ) ;
+            writeAnswer( meta.getProductName() ) ;
+        }
+        
+        for( AWSProductMeta meta : metaList ) {
+            writeQuestion( "What does this product do??\n\n**" + meta.getProductName() + "**" ) ;
+            writeAnswer( meta.getShortDescription() ) ;
+        }
+        
         fw.flush() ;
         fw.close() ;
+    }
+    
+    private File getJNFile( String category, int subChapNum ) {
+        
+        String fileName = "1." + subChapNum + " - " ;
+        fileName += category.replace( "&", "and" ) ;
+        fileName += ".jn" ;
+        
+        File jnFile = new File( jnFolder, fileName ) ;
+        return jnFile ;
+    }
+    
+    private void createMatchingQuestion( List<AWSProductMeta> metaList )
+        throws Exception {
+        
+        fw.write( "\n@match \"Match the following\" {\n" ) ;
+        
+        for( AWSProductMeta meta : metaList ) {
+            fw.write( "\t\"" + meta.getProductName() + "\" = \"" + meta.getShortDescription() + "\"\n" ) ;
+        }
+        
+        fw.write( "\n" ) ;
+        fw.write( "\t@mcq_config {\n" ) ;
+        fw.write( "\t\t@forwardCaption \"What does this product do?\"\n" ) ;
+        fw.write( "\t\t@reverseCaption \"Which product does this?\"\n" ) ;
+        fw.write( "\t\t@numOptionsToShow 4\n" ) ;
+        fw.write( "\t\t@numOptionsPerRow 1\n" ) ;
+        fw.write( "\t}\n" ) ;
+        fw.write( "}\n" ) ;
+     }
+    
+    private void writeQuestion( String question ) throws Exception {
+        fw.write( "\n" ) ;
+        fw.write( "@qa \"" + question + "\"\n" ) ;
+    }
+    
+    private void writeAnswer( String answer ) throws Exception {
+        fw.write( "\"" + answer + "\"\n" ) ;
     }
     
     @SuppressWarnings( { "resource", "unchecked" } )
