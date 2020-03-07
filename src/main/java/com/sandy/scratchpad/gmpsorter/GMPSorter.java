f71e267fc60beb47ec444672b0a3cd3e04182a6d
Sandeep Deb <deb.sandeep@gmail.com>
2020-03-07 18:04:21 +0530

    Simple

--- src/main/java/com/sandy/scratchpad/gmpsorter/GMPSorter.java
+++ src/main/java/com/sandy/scratchpad/gmpsorter/GMPSorter.java
@@ -19,8 +19,8 @@ import com.sandy.scratchpad.jn.imgsorter.ThumbnailViewer ;
 public class GMPSorter extends JFrame implements ListSelectionListener {
     
     public static final String BOOK_SHORT_NAME = "AITS" ;
-    public static final String SUBJECT_FOLDER_NAME = "IIT - Chemistry" ;
-    public static final String IMG_PREFIX = "Chem_Q_FJ" ;
+    public static final String SUBJECT_FOLDER_NAME = "IIT - Maths" ;
+    public static final String IMG_PREFIX = "Math_Q_FJ" ;
     
     private class TopicShortcutProcessor extends Thread {
         
