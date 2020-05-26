1a24bcede06b3a00642087259f709f4899958b58
Sandeep Deb <deb.sandeep@gmail.com>
2020-05-26 14:11:30 +0530

    Trivia

--- src/main/java/com/sandy/scratchpad/six2tenbooks/BookList.java
+++ src/main/java/com/sandy/scratchpad/six2tenbooks/BookList.java
@@ -2,7 +2,6 @@ package com.sandy.scratchpad.six2tenbooks;
 
 import java.io.BufferedReader ;
 import java.io.File ;
-import java.io.IOException ;
 import java.io.InputStreamReader ;
 import java.util.ArrayList ;
 import java.util.Arrays ;
