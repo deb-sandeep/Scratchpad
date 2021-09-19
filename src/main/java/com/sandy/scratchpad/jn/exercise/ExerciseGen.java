9bf1e62f816767e099205f141262fc8ca80698a5
Sandeep Deb <deb.sandeep@gmail.com>
2021-09-19 18:04:54 +0530

    Config tweaks

--- src/main/java/com/sandy/scratchpad/jn/exercise/ExerciseGen.java
+++ src/main/java/com/sandy/scratchpad/jn/exercise/ExerciseGen.java
@@ -340,9 +340,9 @@ public class ExerciseGen {
         String JN_ROOT_DIR      = "/Users/sandeep/Documents/StudyNotes/JoveNotes-Std-7/" ;
         String JN_CLS_DIR       = "Class-7" ;
         String JN_SUBJECT       = "Mathematics" ;
-        String JN_CHAPTER       = "03 - Fractions" ;
-        String JN_BASE_CHP_NAME = "Fractions" ;
-        int    JN_CHAPTER_NUM   = 3 ;
+        String JN_CHAPTER       = "07 - Unitary Method" ;
+        String JN_BASE_CHP_NAME = "Unitary Method" ;
+        int    JN_CHAPTER_NUM   = 7 ;
         int    JN_SUB_CHP_START = 2 ;
         String includedExercises[]  = {} ;
 
